8181edcc20b121cb304a6c831c878a15be1c7b57
JaeOhHa <gkwodhsla@naver.com>
2020-05-23 17:24:13 +0900

    makeGUI(1)
    step three make GUI

--- GUI.py
+++ GUI.py
@@ -14,10 +14,14 @@ class GUI:
         self.window.geometry("800x600")
         self.font1 = Font(family="맑은 고딕", size=20, slant="italic")
         self.font2 = Font(family="Arial", size=10)
+        self.font3 = Font(family="굴림",size=12)
 
         Label(text="전기자동차 공공충전소 현황", font=self.font1).pack()
         self.initButton()
         self.initListBox()
+        self.mapLabel=Label(text='이곳에 지도가 그려질 예정입니다.',font=self.font1,bg='white')
+        #지도는 html frame이라는걸 사용하면 될 것 같습니다.
+        self.mapLabel.place(x=350, y=100)
         self.window.mainloop()
 
     def initButton(self):
@@ -51,10 +55,11 @@ class GUI:
 
         for i in range(len(xmlProcessing.locations)):
             self.locationListBox.insert(i, xmlProcessing.locations[i])
+        Label(self.window,text="지역 리스트",font=self.font3).place(x=40,y=340)
         # 여기까지 지역 리스트 만드는 코드
 
         self.frame2 = Frame(self.window)
-        self.frame2.place(x=10, y=350)
+        self.frame2.place(x=10, y=400)
         self.stationListBox = Listbox(self.frame2, selectmode='extended',
                                       height=7, width=40, font=self.font2,
                                       relief='ridge', borderwidth=7)
@@ -64,7 +69,23 @@ class GUI:
         self.scrollbarWithSpecificList.config(command=self.stationListBox.yview)
         self.scrollbarWithSpecificList.pack(side="right", fill="y")
         self.stationListBox.config(yscrollcommand=self.scrollbarWithSpecificList.set)
+        Label(self.window, text="지역 내 충전소 리스트", font=self.font3).place(x=70,y=550)
         # 여기까지 지역 스테이션 리스트박스 만들기
+
+        self.frame3 = Frame(self.window)
+        self.frame3.place(x=350, y=400)
+        self.specificInfoList = Listbox(self.frame3, selectmode='extended',
+                                      height=7, width=55, font=self.font2,
+                                      relief='ridge', borderwidth=7)
+        self.specificInfoList.pack(side="left", fill="y")
+
+        self.scrollbarWithSpecificInfoList = Scrollbar(self.frame3, orient="vertical")
+        self.scrollbarWithSpecificInfoList.config(command=self.specificInfoList.yview)
+        self.scrollbarWithSpecificInfoList.pack(side="right", fill="y")
+        self.specificInfoList.config(yscrollcommand=self.scrollbarWithSpecificInfoList.set)
+        Label(self.window, text="충전소 세부사항", font=self.font3).place(x=500, y=550)
+        #여기까지 스테이션의 세부사항 리스트.
+
 
     def getStationList(self):
         self.currentSelectedLocation = self.locationListBox.curselection()
