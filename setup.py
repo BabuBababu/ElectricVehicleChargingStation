77eafa068d41737207968ee95087c939f20c0670
BabuBababu <poryou77@gmail.com>
2020-06-15 22:16:44 +0900

    Make package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ElectricVehicle',
+    version='1.0',
+    description='Project for ScriptLanguage',
+    author='Minsoo Cho,Jaeoh Ha',
+    author_email='poryou66@gmail.com',
+    url='https://github.com/BabuBababu/ElectricVehicleChargingStation',
+    packages=['','img'],
+    package_data={'':['*'],'img':['*']}
+)
